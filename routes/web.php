c0c186ebfbb4617dee96c095d846963d5b65424a
Homero Raul <h.raulvc@gmail.com>
2023-01-24 23:57:37 -0600

    24012023

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,40 @@
 
 use Illuminate\Support\Facades\Route;
 
+use LaravelersAcademy\ZoomMeeting\Facades\Meeting;
 
 Route::get('/', function () {
 
-    $res = Calculator::add(5)->add(8)->getResult();
+
+    $account = '7czFSoA-REqftiQFvFSqhg';
+
+    $client = 'VRSGjLeiRuozqgAu4FLzg';
+
+    $secret = 'o6bYtv44x54ToiuuB8tYJ7sIXEFB7xqM';
+
+    $meetingId = 89217991969;
+
+    dd(Meeting::get($meetingId));
+
+    //////////////////////////
+    //////////////////////////
+    //////////////////////////
+
+    try {
+
+        $res = Meeting::set([
+            'account' => $account,
+            'client' => $client,
+            'secret' => $secret
+        ])->create([
+            'topic' => 'Mi reunión de la api de paquete'
+        ]);
+
+    } catch( \Exception $e) {
+
+        dd($e);
+
+    }
 
     dd($res);
 
